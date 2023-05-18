1bdcd364d5829f4e0add31fcbac9e3eba8294f7b
Qiusheng Wu <giswqs@gmail.com>
2023-05-17 22:21:20 -0400

    Add SamGeo tests

--- tests/test_samgeo.py
+++ tests/test_samgeo.py
@@ -2,7 +2,7 @@
 
 """Tests for `samgeo` package."""
 
-
+import os
 import unittest
 
 from samgeo import samgeo
@@ -13,9 +13,32 @@ class TestSamgeo(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        bbox = [-122.2659, 37.8682, -122.2521, 37.8741]
+        image = "satellite.tif"
+        samgeo.tms_to_geotiff(
+            output=image, bbox=bbox, zoom=17, source="Satellite", overwrite=True
+        )
+        self.source = image
+
+        out_dir = os.path.join(os.path.expanduser("~"), "Downloads")
+        checkpoint = os.path.join(out_dir, "sam_vit_h_4b8939.pth")
+
+        sam = samgeo.SamGeo(
+            model_type="vit_h",
+            checkpoint=checkpoint,
+            sam_kwargs=None,
+        )
+
+        self.sam = sam
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_generate(self):
         """Test something."""
+
+        sam = self.sam
+        source = self.source
+
+        sam.generate(source, output="masks.tif", foreground=True, unique=True)
+        self.assertTrue(os.path.exists("masks.tif"))
