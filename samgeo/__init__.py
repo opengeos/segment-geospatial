238e4433e3b5b8658b3a44c76ff3627049dca712
Qiusheng Wu <giswqs@gmail.com>
2023-08-06 15:19:19 -0400

    Update init.py

--- samgeo/__init__.py
+++ samgeo/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for segment-geospatial."""
 
 __author__ = """Qiusheng Wu"""
-__email__ = 'giswqs@gmail.com'
-__version__ = '0.8.5'
+__email__ = "giswqs@gmail.com"
+__version__ = "0.8.5"
 
 
 from .samgeo import *
