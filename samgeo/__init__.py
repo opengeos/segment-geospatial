e018ff1a12e2a83f634b3818a25f38743a7e35df
Qiusheng Wu <giswqs@gmail.com>
2023-08-06 15:24:29 -0400

    Fixed bumpversion error (#160)
    * Update notebook
    
    * Update init.py
    
    * Update setup.py
    
    * Update init.py

--- samgeo/__init__.py
+++ samgeo/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for segment-geospatial."""
 
 __author__ = """Qiusheng Wu"""
-__email__ = 'giswqs@gmail.com'
+__email__ = "giswqs@gmail.com"
 __version__ = '0.8.5'
 
 
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opengeos/segment-geospatial",
-    version="0.8.5",
+    version='0.8.5',
     zip_safe=False,
 )
