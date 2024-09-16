f4144aaeb5f8034039b81f48e4809ea66ebfebb0
Qiusheng Wu <giswqs@gmail.com>
2024-09-16 19:18:52 -0400

    Bump version: 0.10.8 → 0.11.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opengeos/segment-geospatial",
-    version="0.10.8",
+    version="0.11.0",
     zip_safe=False,
 )
