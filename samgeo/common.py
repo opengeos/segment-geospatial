5dc9727309d9954ba7c734b67e89e97ed4d258cb
Qiusheng Wu <giswqs@gmail.com>
2024-10-13 21:35:40 -0400

    Add support for box prompts for SamGeo2

--- samgeo/common.py
+++ samgeo/common.py
@@ -406,13 +406,12 @@ def tms_to_geotiff(
 
     """
 
-    import os
+    import re
     import io
     import math
     import itertools
     import concurrent.futures
 
-    import numpy
     from PIL import Image
 
     try:
@@ -490,7 +489,18 @@ def tms_to_geotiff(
 
     gdal.UseExceptions()
     web_mercator = osr.SpatialReference()
-    web_mercator.ImportFromEPSG(3857)
+    try:
+        web_mercator.ImportFromEPSG(3857)
+    except RuntimeError as e:
+        # https://github.com/PDAL/PDAL/issues/2544#issuecomment-637995923
+        if "PROJ" in str(e):
+            pattern = r"/[\w/]+"
+            match = re.search(pattern, str(e))
+            if match:
+                file_path = match.group(0)
+                os.environ["PROJ_LIB"] = file_path
+                os.environ["GDAL_DATA"] = file_path.replace("proj", "gdal")
+                web_mercator.ImportFromEPSG(3857)
 
     WKT_3857 = web_mercator.ExportToWkt()
 
