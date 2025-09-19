6b125a0226f48085924835def27d70d05d984d03
Moreno Castaño, Guillermo <gmcastano@indra.es>
2025-09-19 11:52:18 +0200

    chore: Refactor package requirements

--- samgeo/__init__.py
+++ samgeo/__init__.py
@@ -5,6 +5,5 @@ __email__ = "giswqs@gmail.com"
 __version__ = "0.13.0"
 
 
-from .common import show_image_gui  # noqa: F401
 from .samgeo import *  # noqa: F403
 from .samgeo2 import *  # noqa: F403
